8400dbffadda97d0245f78a8c0776fada3dd5aeb
knadejpeg <mercedesmaria.vitali@mail.polimi.it>
2021-12-17 11:47:19 +0100

    V1
    Upload

---
+++ public/sketch.js
@@ -0,0 +1,84 @@
+// server - client variables
+
+let clientSocket = io();
+clientSocket.on("connect", newConnection);
+
+// sketch variables
+
+let fillColor = [
+'red', 'pink', 'purple', 'orange', 'blue'
+];
+let img;
+
+
+// server communication 
+
+function preload () {
+  img = loadImage("./assets/quadretti_bg.jpg")
+  console.log('image loaded');
+}
+
+function newConnection() {
+  console.log("your id:", clientSocket.id);
+} 
+
+
+// sketch
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  image(img, -50, -50, 1920, 1080); // bg image, stays fixed
+  noStroke();
+  fill(random(fillColor)); // in setup to not change at every frame 
+  frameRate(3);
+
+  let myText1 = "write a kind message to make a stranger happy";
+
+  drawingContext.font = "42px Lora";
+  fill('pink');
+  textLeading(52);
+  text(myText1, windowWidth*1/24, windowHeight*1/24, 500, 500);
+  
+}
+
+
+// socket 
+
+clientSocket.on('mouseBroadcast', otherMouse);
+
+function otherMouse(dataReceived) {
+  fill(fillColor);
+  circle(dataReceived.x, dataReceived.y, 10); // part of the sketch 
+}
+
+function mouseDragged () {
+  console.log (mouseX + ',' + mouseY);
+  noStroke();
+  fill(fillColor);
+  circle(mouseX, mouseY, 10); 
+
+  let message = {
+    id: clientSocket.id,
+    x: mouseX,y: mouseY,
+  };
+clientSocket.emit('mouse', message);
+}
+
+
+
+
+// sketch
+
+
+
+// color changes when there is a new click
+function mouseClicked() {
+  fill(random(fillColor));
+  noStroke ();
+ }
+ 
+
+// sketch - text
+
+
+
---
+++ server.js
@@ -0,0 +1,35 @@
+const { Socket } = require("engine.io");
+
+let express = require('express');
+let app = express();
+let port = 3000;
+let server = app.listen(port);
+
+console.log("server up and running");
+
+
+app.use(express.static('public'));
+
+let serverSocket = require('socket.io');
+
+let io = serverSocket(server);
+
+io.on('connection', newConnection);
+
+console.log("server up and running");
+console.log("running server on http://localhost:" + port);
+
+function newConnection(newSocket) {
+    console.log("new connection:", newSocket.id);
+    newSocket.on("mouse", incomingMouseMessage);
+
+    function incomingMouseMessage(dataReceived){
+        console.log(dataReceived);
+        newSocket.broadcast.emit("mouseBroadcast", dataReceived);
+  }
+  
+ 
+}
+
+  
+  
